9951f51bc7098d4f3814bc931efb36a62eeb387c
FujiApple <fujiapple852@gmail.com>
2022-04-12 19:42:29 +0800

    chore: allow `clippy::missing_const_for_fn`

--- src/icmp/tracer.rs
+++ src/icmp/tracer.rs
@@ -59,7 +59,7 @@ pub struct IcmpTracer<F> {
 }
 
 impl<F: Fn(&Probe)> IcmpTracer<F> {
-    pub const fn new(config: &IcmpTracerConfig, publish: F) -> Self {
+    pub fn new(config: &IcmpTracerConfig, publish: F) -> Self {
         Self {
             target_addr: config.target_addr,
             trace_identifier: config.trace_identifier,
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 //! Trippy, a network diagnostic tool.
 //!
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, rust_2018_idioms)]
-#![allow(clippy::module_name_repetitions, clippy::option_if_let_else)]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::option_if_let_else,
+    clippy::missing_const_for_fn
+)]
 #![forbid(unsafe_code)]
 
 mod icmp;
