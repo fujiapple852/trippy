9d49d3a7a84a78fac34a674b2460c7331fc7c67c
FujiApple <fujiapple852@gmail.com>
2022-04-19 12:09:21 +0800

    feat: adjusted argument short option names

--- src/config.rs
+++ src/config.rs
@@ -16,11 +16,11 @@ pub struct Args {
     pub hostname: String,
 
     /// The TTL to start from
-    #[clap(short, long, default_value_t = 1)]
+    #[clap(long, default_value_t = 1)]
     pub first_ttl: u8,
 
     /// The maximum number of hops
-    #[clap(short, long, default_value_t = 64, max_values(256))]
+    #[clap(short = 't', long, default_value_t = 64, max_values(256))]
     pub max_ttl: u8,
 
     /// The minimum duration of every round
@@ -28,7 +28,7 @@ pub struct Args {
     pub min_round_duration: String,
 
     /// The maximum duration of every round
-    #[clap(short = 't', long, default_value = "1s")]
+    #[clap(short = 'I', long, default_value = "1s")]
     pub max_round_duration: String,
 
     /// The period of time to wait for additional ICMP responses after the target has responded
@@ -40,7 +40,7 @@ pub struct Args {
     pub max_inflight: u8,
 
     /// The socket read timeout.
-    #[clap(short, long, default_value = "10ms")]
+    #[clap(long, default_value = "10ms")]
     pub read_timeout: String,
 
     /// Preserve the screen on exit
