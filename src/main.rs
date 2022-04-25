0e94427fc186e1bb2c0b83b4ccf71e1a89f94dc6
FujiApple <fujiapple852@gmail.com>
2022-04-25 14:24:48 +0800

    chore: allow `clippy::redundant_pub_crate` lint

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@
     clippy::missing_const_for_fn,
     clippy::cast_precision_loss,
     clippy::cast_sign_loss,
-    clippy::cast_possible_truncation
+    clippy::cast_possible_truncation,
+    clippy::redundant_pub_crate
 )]
 #![forbid(unsafe_code)]
 
