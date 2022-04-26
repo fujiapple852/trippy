7c1e468fa9cc151f12a5fd8a4f86f35a6338dec1
FujiApple <fujiapple852@gmail.com>
2022-04-26 17:39:00 +0800

    doc: fix documentation for new-types

--- src/tracing/types.rs
+++ src/tracing/types.rs
@@ -1,34 +1,34 @@
 use derive_more::{Add, AddAssign, From, Rem, Sub};
 
-/// Round newtype.
+/// `Round` newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From, AddAssign)]
 pub struct Round(pub usize);
 
-/// Time-to-live (ttl) newtype.
+/// `TimeToLive` (ttl) newtype.
 #[derive(
     Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From, Add, Sub, AddAssign,
 )]
 pub struct TimeToLive(pub u8);
 
-/// Sequence number newtype.
+/// `Sequence` number newtype.
 #[derive(
     Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From, Add, Sub, AddAssign, Rem,
 )]
 pub struct Sequence(pub u16);
 
-/// Trace Identifier newtype.
+/// `TraceId` newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From)]
 pub struct TraceId(pub u16);
 
-/// Max Inflight newtype.
+/// `MaxInflight` newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From)]
 pub struct MaxInflight(pub u8);
 
-/// Trace Identifier newtype.
+/// `PacketSize` newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From)]
 pub struct PacketSize(pub u16);
 
-/// Max Inflight newtype.
+/// `PayloadPattern` newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From)]
 pub struct PayloadPattern(pub u8);
 
@@ -41,3 +41,9 @@ impl From<Sequence> for usize {
 /// Source port newtype.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Ord, PartialOrd, From)]
 pub struct SourcePort(pub u16);
+
+impl From<Sequence> for usize {
+    fn from(sequence: Sequence) -> Self {
+        sequence.0 as Self
+    }
+}
