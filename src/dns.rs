f76dada793a8a9f1a0a9cf31c28d85bb6f03d053
FujiApple <fujiapple852@gmail.com>
2022-04-29 17:11:56 +0800

    feat: added AS (autonomous system) lookup capability to `DnsResolver`

--- src/dns.rs
+++ src/dns.rs
@@ -10,8 +10,8 @@ use std::time::Duration;
 pub enum DnsEntry {
     /// The reverse DNS resolution of `IpAddr` is pending.
     Pending(IpAddr),
-    /// The reverse DNS resolution of `IpAddr` has resolved as `String`.
-    Resolved(IpAddr, Vec<String>),
+    /// The reverse DNS resolution of `IpAddr` has resolved.
+    Resolved(Resolved),
     /// The `IpAddr` could not be resolved.
     NotFound(IpAddr),
     /// The reverse DNS resolution of `IpAddr` failed.
@@ -20,15 +20,56 @@ pub enum DnsEntry {
     Timeout(IpAddr),
 }
 
+/// Information about a resolved `IpAddr`.
+#[derive(Debug, Clone)]
+pub enum Resolved {
+    /// Resolved without AsInfo.
+    Normal(IpAddr, Vec<String>),
+    /// Resolved with AsInfo.
+    WithAsInfo(IpAddr, Vec<String>, AsInfo),
+}
+
 impl Display for DnsEntry {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         #[allow(clippy::match_same_arms)]
         match self {
-            DnsEntry::Resolved(_, s) => write!(f, "{}", s.join(" ")),
+            DnsEntry::Resolved(Resolved::Normal(_, hosts)) => write!(f, "{}", hosts.join(" ")),
+            DnsEntry::Resolved(Resolved::WithAsInfo(_, hosts, asinfo)) => {
+                write!(f, "AS{} {}", asinfo.asn, hosts.join(" "))
+            }
             DnsEntry::Pending(ip) => write!(f, "{}", ip),
             DnsEntry::NotFound(ip) => write!(f, "{}", ip),
             DnsEntry::Failed(ip) => write!(f, "Failed: {}", ip),
             DnsEntry::Timeout(ip) => write!(f, "Timeout: {}", ip),
+        }
+    }
+}
+
+/// Autonomous System (AS) information.
+#[derive(Debug, Clone, Default)]
+pub struct AsInfo {
+    pub asn: String,
+    pub prefix: String,
+    pub cc: String,
+    pub registry: String,
+    pub allocated: String,
+    pub name: String,
+}
+
+/// Configuration for the `DnsResolver`.
+#[derive(Debug, Copy, Clone)]
+pub struct DnsResolverConfig {
+    pub resolve_method: DnsResolveMethod,
+    pub timeout: Duration,
+    pub lookup_as_info: bool,
+}
+
+impl DnsResolverConfig {
+    pub fn new(resolve_method: DnsResolveMethod, timeout: Duration, lookup_as_info: bool) -> Self {
+        Self {
+            resolve_method,
+            timeout,
+            lookup_as_info,
         }
     }
 }
@@ -40,9 +81,9 @@ pub struct DnsResolver {
 }
 
 impl DnsResolver {
-    pub fn new(resolve_method: DnsResolveMethod, timeout: Duration) -> Self {
+    pub fn new(config: DnsResolverConfig) -> Self {
         Self {
-            inner: Rc::new(DnsResolverInner::new(resolve_method, timeout)),
+            inner: Rc::new(DnsResolverInner::new(config)),
         }
     }
 
@@ -68,16 +109,20 @@ impl DnsResolver {
 
 /// Private impl of resolver.
 mod inner {
-    use crate::dns::{DnsEntry, DnsResolveMethod};
+    use crate::dns::{AsInfo, DnsEntry, DnsResolveMethod, DnsResolverConfig, Resolved};
+    use anyhow::anyhow;
     use crossbeam::channel::{bounded, Receiver, Sender};
+    use itertools::Itertools;
     use parking_lot::RwLock;
     use std::collections::HashMap;
-    use std::net::IpAddr;
+    use std::net::{IpAddr, Ipv4Addr};
+    use std::str::FromStr;
     use std::sync::Arc;
     use std::thread;
     use std::time::Duration;
     use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
     use trust_dns_resolver::error::ResolveErrorKind;
+    use trust_dns_resolver::proto::rr::RecordType;
     use trust_dns_resolver::{Name, Resolver};
 
     /// The maximum number of in-flight reverse DNS resolutions that may be
@@ -103,16 +148,16 @@ mod inner {
     }
 
     impl DnsResolverInner {
-        pub fn new(method: DnsResolveMethod, timeout: Duration) -> Self {
+        pub fn new(config: DnsResolverConfig) -> Self {
             let (tx, rx) = bounded(RESOLVER_MAX_QUEUE_SIZE);
             let addr_cache = Arc::new(RwLock::new(HashMap::new()));
 
-            let provider = if let DnsResolveMethod::System = method {
+            let provider = if let DnsResolveMethod::System = config.resolve_method {
                 DnsProvider::DnsLookup
             } else {
                 let mut options = ResolverOpts::default();
-                options.timeout = timeout;
-                let res = match method {
+                options.timeout = config.timeout;
+                let res = match config.resolve_method {
                     DnsResolveMethod::Resolv => Resolver::from_system_conf(),
                     DnsResolveMethod::Google => Resolver::new(ResolverConfig::google(), options),
                     DnsResolveMethod::Cloudflare => {
@@ -128,7 +173,9 @@ mod inner {
             {
                 let cache = addr_cache.clone();
                 let provider = provider.clone();
-                thread::spawn(move || resolver_queue_processor(rx, &provider, &cache));
+                thread::spawn(move || {
+                    resolver_queue_processor(rx, &provider, &cache, config.lookup_as_info)
+                });
             }
             Self {
                 provider,
@@ -196,20 +243,31 @@ mod inner {
     ///
     /// For each `IpAddr`, perform the reverse DNS lookup and update the cache with the result (`Resolved`, `NotFound`,
     /// `Timeout` or `Failed`) for that addr.
-    fn resolver_queue_processor(rx: Receiver<IpAddr>, provider: &DnsProvider, cache: &Cache) {
+    fn resolver_queue_processor(
+        rx: Receiver<IpAddr>,
+        provider: &DnsProvider,
+        cache: &Cache,
+        asinfo_lookup: bool,
+    ) {
         for addr in rx {
             let entry = match &provider {
                 DnsProvider::DnsLookup => {
                     // we can't distinguish between a failed lookup or a genuine error and so we just assume all
                     // failures are `DnsEntry::NotFound`.
                     match dns_lookup::lookup_addr(&addr) {
-                        Ok(dns) => DnsEntry::Resolved(addr, vec![dns]),
+                        Ok(dns) => DnsEntry::Resolved(Resolved::Normal(addr, vec![dns])),
                         Err(_) => DnsEntry::NotFound(addr),
                     }
                 }
                 DnsProvider::TrustDns(resolver) => match resolver.reverse_lookup(addr) {
                     Ok(name) => {
-                        DnsEntry::Resolved(addr, name.iter().map(Name::to_string).collect())
+                        let hostnames = name.iter().map(Name::to_string).collect();
+                        if asinfo_lookup {
+                            let as_info = lookup_asinfo(resolver, addr).unwrap_or_default();
+                            DnsEntry::Resolved(Resolved::WithAsInfo(addr, hostnames, as_info))
+                        } else {
+                            DnsEntry::Resolved(Resolved::Normal(addr, hostnames))
+                        }
                     }
                     Err(err) => match err.kind() {
                         ResolveErrorKind::NoRecordsFound { .. } => DnsEntry::NotFound(addr),
@@ -220,5 +278,100 @@ mod inner {
             };
             cache.write().insert(addr, entry);
         }
+    }
+
+    /// Lookup up `AsInfo` for an `IpAddr` address.
+    fn lookup_asinfo(resolver: &Arc<Resolver>, addr: IpAddr) -> anyhow::Result<AsInfo> {
+        match addr {
+            IpAddr::V4(addr) => lookup_asinfo_ipv4(resolver, addr),
+            IpAddr::V6(_) => Ok(AsInfo::default()),
+        }
+    }
+
+    /// Lookup up `AsInfo` for an `Ipv4Addr` address.
+    fn lookup_asinfo_ipv4(resolver: &Arc<Resolver>, addr: Ipv4Addr) -> anyhow::Result<AsInfo> {
+        let origin_query_txt = query_asn_ipv4(resolver, addr)?;
+        let asinfo = parse_origin_query_txt(&origin_query_txt)?;
+        let asn_query_txt = query_asn_name_ipv4(resolver, &asinfo.asn)?;
+        let as_name = parse_asn_query_txt(&asn_query_txt)?;
+        Ok(AsInfo {
+            asn: asinfo.asn,
+            prefix: asinfo.prefix,
+            cc: asinfo.cc,
+            registry: asinfo.registry,
+            allocated: asinfo.allocated,
+            name: as_name,
+        })
+    }
+
+    /// Perform the `origin` query.
+    fn query_asn_ipv4(resolver: &Arc<Resolver>, addr: Ipv4Addr) -> anyhow::Result<String> {
+        let query = format!(
+            "{}.origin.asn.cymru.com.",
+            addr.octets().iter().rev().join(".")
+        );
+        let name = Name::from_str(query.as_str())?;
+        let response = resolver.lookup(name, RecordType::TXT)?;
+        let data = response
+            .iter()
+            .next()
+            .ok_or_else(|| anyhow!("asn origin query"))?;
+        let bytes = data.as_txt().ok_or_else(|| anyhow!("asn origin query"))?;
+        Ok(bytes.to_string())
+    }
+
+    /// Perform the `asn` query.
+    fn query_asn_name_ipv4(resolver: &Arc<Resolver>, asn: &str) -> anyhow::Result<String> {
+        let query = format!("AS{}.asn.cymru.com.", asn);
+        let name = Name::from_str(query.as_str())?;
+        let response = resolver.lookup(name, RecordType::TXT)?;
+        let data = response.iter().next().ok_or_else(|| anyhow!("asn query"))?;
+        let bytes = data.as_txt().ok_or_else(|| anyhow!("asn query"))?;
+        Ok(bytes.to_string())
+    }
+
+    /// The `origin` DNS query returns a TXT record in the formal:
+    ///      `asn | prefix | cc | registry | allocated`
+    ///
+    /// For example:
+    ///      `12301 | 81.0.100.0/22 | HU | ripencc | 2001-12-06`
+    ///
+    /// From this we extract all fields.
+    fn parse_origin_query_txt(origin_query_txt: &str) -> anyhow::Result<AsInfo> {
+        if origin_query_txt.chars().filter(|c| *c == '|').count() != 4 {
+            return Err(anyhow!(
+                "failed to parse AS origin txt: {}",
+                origin_query_txt
+            ));
+        }
+        let mut split = origin_query_txt.split('|');
+        let asn = split.next().unwrap_or_default().trim().to_string();
+        let prefix = split.next().unwrap_or_default().trim().to_string();
+        let cc = split.next().unwrap_or_default().trim().to_string();
+        let registry = split.next().unwrap_or_default().trim().to_string();
+        let allocated = split.next().unwrap_or_default().trim().to_string();
+        Ok(AsInfo {
+            asn,
+            prefix,
+            cc,
+            registry,
+            allocated,
+            name: String::default(),
+        })
+    }
+
+    /// The `asn` DNS query returns a TXT record in the formal:
+    ///      `asn | cc | registry | allocated | name`
+    ///
+    /// For example:
+    ///      `12301 | HU | ripencc | 1999-02-25 | INVITECH, HU`
+    ///
+    /// From this we extract the 4th field (name, `INVITECH, HU` in this example)
+    fn parse_asn_query_txt(asn_query_txt: &str) -> anyhow::Result<String> {
+        if asn_query_txt.chars().filter(|c| *c == '|').count() != 4 {
+            return Err(anyhow!("failed to parse AS origin txt: {}", asn_query_txt));
+        }
+        let mut split = asn_query_txt.split('|');
+        Ok(split.nth(4).unwrap_or_default().trim().to_string())
     }
 }
