de9892413d35e903fe6a7a26143a7e41aaf6a49c
FujiApple <fujiapple852@gmail.com>
2022-05-18 14:59:38 +0800

    feat: added `-4` (``--ipv4`) and `-6` (`--ipv6`) command line arguments

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
     clippy::cast_precision_loss,
     clippy::cast_sign_loss,
     clippy::cast_possible_truncation,
-    clippy::redundant_pub_crate
+    clippy::redundant_pub_crate,
+    clippy::struct_excessive_bools
 )]
 #![forbid(unsafe_code)]
 
