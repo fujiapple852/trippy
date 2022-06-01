9a71b0aa30fdfe1fca76c47628e032b94678f94b
FujiApple <fujiapple852@gmail.com>
2022-06-01 20:56:37 +0800

    refactor(net): move `Network` to the top of `net.rs` module

--- src/tracing/net.rs
+++ src/tracing/net.rs
@@ -22,6 +22,17 @@ mod platform;
 /// A channel for sending and receiving probes.
 pub mod channel;
 
+/// An abstraction over a network interface for tracing.
+pub trait Network {
+    /// Send a `Probe`.
+    fn send_probe(&mut self, probe: Probe) -> TraceResult<()>;
+
+    /// Receive the next Icmp packet and return a `ProbeResponse`.
+    ///
+    /// Returns `None` if the read times out or the packet read is not one of the types expected.
+    fn recv_probe(&mut self) -> TraceResult<Option<ProbeResponse>>;
+}
+
 /// The response to a probe.
 #[derive(Debug, Copy, Clone)]
 pub enum ProbeResponse {
@@ -64,17 +75,6 @@ impl TcpProbeResponseData {
     fn new(recv: SystemTime, addr: IpAddr, ttl: u8) -> Self {
         Self { recv, addr, ttl }
     }
-}
-
-/// An abstraction over a network interface for tracing.
-pub trait Network {
-    /// Send a `Probe`
-    fn send_probe(&mut self, probe: Probe) -> TraceResult<()>;
-
-    /// Receive the next Icmp packet and return a `ProbeResponse`.
-    ///
-    /// Returns `None` if the read times out or the packet read is not one of the types expected.
-    fn recv_probe(&mut self) -> TraceResult<Option<ProbeResponse>>;
 }
 
 /// The port used for local address discovery if not dest port is available.
