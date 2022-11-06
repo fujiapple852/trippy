087f9581be80cb95042da768778973203c38aae2
Benjamin Ryzman <benjamin@ryzman.fr>
2022-11-06 19:34:58 +0100

    Find address for specified adapter name

--- src/tracing/net/platform/windows.rs
+++ src/tracing/net/platform/windows.rs
@@ -1,13 +1,13 @@
 use super::byte_order::PlatformIpv4FieldByteOrder;
 use crate::tracing::error::{TraceResult, TracerError};
 use socket2::Socket;
-use windows::Win32::Foundation::{ERROR_SUCCESS, ERROR_BUFFER_OVERFLOW};
+use windows::Win32::Foundation::{NO_ERROR, ERROR_BUFFER_OVERFLOW};
 use std::alloc::{alloc, dealloc, Layout};
 use std::mem;
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{IpAddr};
 use std::time::Duration;
 use windows::Win32::NetworkManagement::IpHelper;
-use windows::Win32::Networking::WinSock::{AF_INET};
+use windows::Win32::Networking::WinSock::{AF_INET, SOCKADDR_IN, inet_ntoa};
 
 /// TODO
 #[allow(clippy::unnecessary_wraps)]
@@ -16,52 +16,64 @@ pub fn for_address(_src_addr: IpAddr) -> TraceResult<PlatformIpv4FieldByteOrder>
 }
 
 /// TODO
-/// inspired by <https://github.com/EstebanBorai/network-interface/blob/main/src/target/windows.rs>
+// inspired by <https://github.com/EstebanBorai/network-interface/blob/main/src/target/windows.rs>
 #[allow(unsafe_code)]
-pub fn lookup_interface_addr_ipv4(_name: &str) -> TraceResult<IpAddr> {
-    /// Max tries allowed to call `GetAdaptersAddresses` on a loop basis
+pub fn lookup_interface_addr_ipv4(name: &str) -> TraceResult<IpAddr> {
+    // Max tries allowed to call `GetAdaptersAddresses` on a loop basis
     const MAX_TRIES: usize = 3;
+    let flags = IpHelper::GAA_FLAG_SKIP_ANYCAST|IpHelper::GAA_FLAG_SKIP_MULTICAST|IpHelper::GAA_FLAG_SKIP_DNS_SERVER;
     // Initial buffer size is 15k per <https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getadaptersaddresses>
     let mut buf_len: u32 = 15000;
+    let mut layout;
+    let mut base_ptr;
+    let mut ip_adapter_address;
+    let mut res;
     let mut i = 0;
-    let flags = IpHelper::GAA_FLAG_SKIP_ANYCAST|IpHelper::GAA_FLAG_SKIP_MULTICAST|IpHelper::GAA_FLAG_SKIP_DNS_SERVER;
-    let mut ip_adapter_address = std::ptr::null_mut();
 
-    let mut res = ERROR_BUFFER_OVERFLOW.0;
-    while res == ERROR_BUFFER_OVERFLOW.0 && i < MAX_TRIES {
-        let layout = Layout::from_size_align(buf_len as usize, mem::align_of::<IpHelper::IP_ADAPTER_ADDRESSES_LH>()).expect("Could not compute layout for buffer size");
-        let base_ptr = unsafe { alloc(layout) };
+    loop {
+        layout = Layout::from_size_align(
+                buf_len as usize,
+                mem::align_of::<IpHelper::IP_ADAPTER_ADDRESSES_LH>()
+            ).expect("Could not compute layout for buffer size");
+        base_ptr = unsafe { alloc(layout) };
         if base_ptr.is_null() {
             return Err(TracerError::SystemError(format!("Could not allocate for layout {:?} of size {} words", layout, buf_len)));
         }
-
         ip_adapter_address = base_ptr.cast();
-        res = unsafe { IpHelper::GetAdaptersAddresses(AF_INET, flags, Some(std::ptr::null_mut()), Some(ip_adapter_address), &mut buf_len) };
-    
-        if res == ERROR_BUFFER_OVERFLOW.0 { // buf_len too small, enlarge
-            unsafe { dealloc(base_ptr, layout) };
-            ip_adapter_address = std::ptr::null_mut();
-        }
-        else {
+
+        res = unsafe { IpHelper::GetAdaptersAddresses(AF_INET, flags, Some(std::ptr::null_mut()), Some(ip_adapter_address), &mut buf_len) };   
+        i += 1;
+
+        if res != ERROR_BUFFER_OVERFLOW.0 || i > MAX_TRIES {
             break;
         }
 
-        i += 1;
+        unsafe { dealloc(base_ptr, layout) };
     }
 
-    if res != ERROR_SUCCESS.0 {
+    if res != NO_ERROR.0 {
         return Err(TracerError::SystemError(format!("GetAdaptersAddresses returned error: {}", res)));
     }
-
-    unsafe {
-        while ! ip_adapter_address.is_null() {
-           let name = (*ip_adapter_address).FriendlyName.to_string().unwrap();
-           println!("{}", name);
-           ip_adapter_address = (*ip_adapter_address).Next;
+    while ! ip_adapter_address.is_null() {
+        let friendly_name = unsafe { (*ip_adapter_address).FriendlyName.to_string().unwrap() };
+        if name ==  friendly_name {
+            // NOTE this really should be a while over the linked list of FistUnicastAddress, and current_unicast would then be mutable
+            // however, this is not supported by our function signature
+            let current_unicast = unsafe { (*ip_adapter_address).FirstUnicastAddress };
+            // while !current_unicast.is_null() {
+                let sockaddr = unsafe { (*current_unicast).Address.lpSockaddr.cast::<SOCKADDR_IN>() };
+                let address = unsafe { inet_ntoa((*sockaddr).sin_addr).to_string().unwrap() };
+                unsafe { dealloc(base_ptr, layout) };
+                return Ok(address.parse().unwrap());
+                // current_unicast = unsafe { (*current_unicast).Next };
+            // }
         }
+        ip_adapter_address = unsafe { (*ip_adapter_address).Next };
     }
 
-    Ok(IpAddr::V4(Ipv4Addr::UNSPECIFIED))
+    unsafe { dealloc(base_ptr, layout) };
+
+    Err(TracerError::UnknownInterface(format!("could not find address for {}", name)))
 }
 
 /// TODO
@@ -132,9 +144,10 @@ pub fn is_conn_refused_error(_code: i32) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::net::{IpAddr, Ipv4Addr};
 
     #[test]
     fn test_ipv4_interface_lookup() {
-        assert!(lookup_interface_addr_ipv4("foo").is_ok());
+        assert_eq!(lookup_interface_addr_ipv4("Ethernet 3").unwrap(), IpAddr::V4(Ipv4Addr::new(192,168,2,2)));
     }
 }
