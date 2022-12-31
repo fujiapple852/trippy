38e37f3684c7ae48d863b5a55817ea7bfc692060
FujiApple <fujiapple852@gmail.com>
2022-12-31 19:36:03 +0000

    refactor(net): unify `TracerSocket` trait

--- src/tracing/net/socket.rs
+++ src/tracing/net/socket.rs
@@ -31,7 +31,6 @@ where
     fn set_ttl(&self, ttl: u32) -> Result<()>;
     fn set_reuse_port(&self, reuse: bool) -> Result<()>;
     fn set_header_included(&self, included: bool) -> Result<()>;
-    fn set_nonblocking(&self, nonblocking: bool) -> Result<()>;
     fn set_unicast_hops_v6(&self, hops: u8) -> Result<()>;
     fn connect(&self, address: SocketAddr) -> Result<()>;
     fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize>;
@@ -39,10 +38,9 @@ where
     fn is_readable(&self, timeout: Duration) -> Result<bool>;
     /// Returns true if the socket is currently writeable, false otherwise.
     fn is_writable(&self) -> Result<bool>;
-    fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, Option<SocketAddr>)>;
+    fn recv_from(&mut self, buf: &mut [u8]) -> Result<(usize, Option<SocketAddr>)>;
     fn read(&mut self, buf: &mut [u8]) -> Result<usize>;
     fn shutdown(&self, how: Shutdown) -> Result<()>;
-    fn local_addr(&self) -> Result<Option<SocketAddr>>;
     fn peer_addr(&self) -> Result<Option<SocketAddr>>;
     fn take_error(&self) -> Result<Option<Error>>;
     fn icmp_error_info(&self) -> Result<IpAddr>;
