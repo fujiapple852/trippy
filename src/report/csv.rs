1db0d59faf5ac325834a429c4cd6e42824ca234b
FujiApple <fujiapple852@gmail.com>
2023-11-24 22:35:47 +0800

    feat(report): reworked `csv` report to use `csv` crate

--- src/report/csv.rs
+++ src/report/csv.rs
@@ -1,17 +1,66 @@
 use crate::backend::trace::Trace;
-use crate::TraceInfo;
+use crate::report::types::fixed_width;
+use crate::{backend, TraceInfo};
 use itertools::Itertools;
-use trippy::dns::{DnsResolver, Resolver};
+use serde::Serialize;
+use std::net::IpAddr;
+use trippy::dns::Resolver;
 
 /// Generate a CSV report of trace data.
-pub fn report(
+pub fn report<R: Resolver>(
     info: &TraceInfo,
     report_cycles: usize,
-    resolver: &DnsResolver,
+    resolver: &R,
 ) -> anyhow::Result<()> {
     let trace = super::wait_for_round(&info.data, report_cycles)?;
-    println!("Target,TargetIp,Hop,IPs,Addrs,Loss%,Snt,Recv,Last,Avg,Best,Wrst,StdDev,");
+    let mut writer = csv::Writer::from_writer(std::io::stdout());
     for hop in trace.hops(Trace::default_flow_id()) {
+        let row = CsvRow::new(&info.target_hostname, info.target_addr, hop, resolver);
+        writer.serialize(row)?;
+    }
+    Ok(())
+}
+
+#[derive(Serialize)]
+pub struct CsvRow {
+    #[serde(rename = "Target")]
+    pub target_hostname: String,
+    #[serde(rename = "TargetIp")]
+    pub target_addr: IpAddr,
+    #[serde(rename = "Hop")]
+    pub ttl: u8,
+    #[serde(rename = "IPs")]
+    pub ip: String,
+    #[serde(rename = "Addrs")]
+    pub host: String,
+    #[serde(rename = "Loss%")]
+    #[serde(serialize_with = "fixed_width")]
+    pub loss_pct: f64,
+    #[serde(rename = "Snt")]
+    pub sent: usize,
+    #[serde(rename = "Recv")]
+    pub recv: usize,
+    #[serde(rename = "Last")]
+    pub last: String,
+    #[serde(rename = "Avg")]
+    #[serde(serialize_with = "fixed_width")]
+    pub avg: f64,
+    #[serde(rename = "Best")]
+    pub best: String,
+    #[serde(rename = "Wrst")]
+    pub worst: String,
+    #[serde(rename = "StdDev")]
+    #[serde(serialize_with = "fixed_width")]
+    pub stddev: f64,
+}
+
+impl CsvRow {
+    fn new<R: Resolver>(
+        target: &str,
+        target_addr: IpAddr,
+        hop: &backend::trace::Hop,
+        resolver: &R,
+    ) -> Self {
         let ttl = hop.ttl();
         let ips = hop.addrs().join(":");
         let ip = if ips.is_empty() {
@@ -39,22 +88,21 @@ pub fn report(
         let stddev = hop.stddev_ms();
         let avg = hop.avg_ms();
         let loss_pct = hop.loss_pct();
-        println!(
-            "{},{},{},{},{},{:.1}%,{},{},{},{:.1},{},{},{:.1}",
-            info.target_hostname,
-            info.target_addr,
+
+        Self {
+            target_hostname: String::from(target),
+            target_addr,
             ttl,
             ip,
             host,
             loss_pct,
             sent,
-            recv,
             last,
+            recv,
             avg,
             best,
             worst,
-            stddev
-        );
+            stddev,
+        }
     }
-    Ok(())
 }
