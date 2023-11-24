c5e6567bd480b21d87828e0e951941e6286aae5a
FujiApple <fujiapple852@gmail.com>
2023-11-24 22:15:37 +0800

    fixup remove comma from stream report

--- src/report/stream.rs
+++ src/report/stream.rs
@@ -27,7 +27,7 @@ pub fn report<R: Resolver>(info: &TraceInfo, resolver: &R) -> anyhow::Result<()>
             let avg = hop.avg;
             let loss_pct = hop.loss_pct;
             println!(
-                "ttl={ttl} addrs={addrs} exts={exts}, loss_pct={loss_pct:.1}, sent={sent} recv={recv} last={last:.1} best={best:.1} worst={worst:.1} avg={avg:.1} stddev={stddev:.1}"
+                "ttl={ttl} addrs={addrs} exts={exts}, loss_pct={loss_pct:.1} sent={sent} recv={recv} last={last:.1} best={best:.1} worst={worst:.1} avg={avg:.1} stddev={stddev:.1}"
             );
         }
         sleep(info.min_round_duration);
