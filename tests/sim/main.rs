45ade312c0e6f65c97afaa5f2628ab6eb5bad2cf
FujiApple <fujiapple852@gmail.com>
2024-01-14 12:23:51 +0800

    build: only use `rust-tun` on linux, macos and windows

--- tests/sim/main.rs
+++ tests/sim/main.rs
@@ -1,4 +1,7 @@
-#![cfg(feature = "sim-tests")]
+#![cfg(all(
+    feature = "sim-tests",
+    any(target_os = "linux", target_os = "macos", target_os = "windows")
+))]
 mod network;
 mod simulation;
 mod tests;
