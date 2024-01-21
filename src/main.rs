9c8cacb45802b7b9480d79b87a3b367095778687
FujiApple <fujiapple852@gmail.com>
2024-01-21 10:49:06 +0800

    chore: ignore `clippy::struct_field_names` clippy lint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, rust_2018_idioms)]
 #![allow(
     clippy::module_name_repetitions,
+    clippy::redundant_field_names,
     clippy::struct_field_names,
     clippy::option_if_let_else,
     clippy::missing_const_for_fn,
