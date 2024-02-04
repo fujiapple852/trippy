cbaa1c1bb6430e5da1769804b22b5c72c5c8ef01
One <43485962+c-git@users.noreply.github.com>
2024-02-03 20:34:17 -0500

    test: switch to using snapshot tool

--- src/main.rs
+++ src/main.rs
@@ -462,21 +462,14 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case(include_str!("../test_resources/tui_theme_items.txt"), &tui_theme_items(); "tui theme items match")]
-    #[test_case(include_str!("../test_resources/tui_binding_commands.txt"), &tui_binding_commands(); "tui binding commands match")]
-    #[test_case(include_str!("../test_resources/completions_bash.txt"), &shell_completions(Shell::Bash).unwrap(); "generate bash shell completions")]
-    #[test_case(include_str!("../test_resources/completions_elvish.txt"), &shell_completions(Shell::Elvish).unwrap(); "generate elvish shell completions")]
-    #[test_case(include_str!("../test_resources/completions_fish.txt"), &shell_completions(Shell::Fish).unwrap(); "generate fish shell completions")]
-    #[test_case(include_str!("../test_resources/completions_powershell.txt"), &shell_completions(Shell::PowerShell).unwrap(); "generate powershell shell completions")]
-    #[test_case(include_str!("../test_resources/completions_zsh.txt"), &shell_completions(Shell::Zsh).unwrap(); "generate zsh shell completions")]
-    fn test_output(expected: &str, actual: &str) {
-        if remove_whitespace(actual.to_string()) != remove_whitespace(expected.to_string()) {
-            pretty_assertions::assert_eq!(actual, expected);
-        }
-    }
-
-    fn remove_whitespace(mut s: String) -> String {
-        s.retain(|c| !c.is_whitespace());
-        s
+    #[test_case("tui_theme_items.txt", &tui_theme_items(); "tui theme items match")]
+    #[test_case("tui_binding_commands.txt", &tui_binding_commands(); "tui binding commands match")]
+    #[test_case("completions_bash.txt", &shell_completions(Shell::Bash).unwrap(); "generate bash shell completions")]
+    #[test_case("completions_elvish.txt", &shell_completions(Shell::Elvish).unwrap(); "generate elvish shell completions")]
+    #[test_case("completions_fish.txt", &shell_completions(Shell::Fish).unwrap(); "generate fish shell completions")]
+    #[test_case("completions_powershell.txt", &shell_completions(Shell::PowerShell).unwrap(); "generate powershell shell completions")]
+    #[test_case("completions_zsh.txt", &shell_completions(Shell::Zsh).unwrap(); "generate zsh shell completions")]
+    fn test_output(snapshot_name: &str, actual: &str) {
+        insta::assert_snapshot!(snapshot_name, actual);
     }
 }
