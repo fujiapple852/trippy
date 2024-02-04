9a7fadf7368b413249a9151aa98ecb9d4f3502c9
One <43485962+c-git@users.noreply.github.com>
2024-02-03 20:48:10 -0500

    test: move tests into a sub-folder

--- src/main.rs
+++ src/main.rs
@@ -458,18 +458,4 @@ fn shell_completions(shell: Shell) -> anyhow::Result<String> {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use test_case::test_case;
-
-    #[test_case("tui_theme_items.txt", &tui_theme_items(); "tui theme items match")]
-    #[test_case("tui_binding_commands.txt", &tui_binding_commands(); "tui binding commands match")]
-    #[test_case("completions_bash.txt", &shell_completions(Shell::Bash).unwrap(); "generate bash shell completions")]
-    #[test_case("completions_elvish.txt", &shell_completions(Shell::Elvish).unwrap(); "generate elvish shell completions")]
-    #[test_case("completions_fish.txt", &shell_completions(Shell::Fish).unwrap(); "generate fish shell completions")]
-    #[test_case("completions_powershell.txt", &shell_completions(Shell::PowerShell).unwrap(); "generate powershell shell completions")]
-    #[test_case("completions_zsh.txt", &shell_completions(Shell::Zsh).unwrap(); "generate zsh shell completions")]
-    fn test_output(snapshot_name: &str, actual: &str) {
-        insta::assert_snapshot!(snapshot_name, actual);
-    }
-}
+mod main_unit_tests;
