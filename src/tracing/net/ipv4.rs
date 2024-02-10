36267d543cd8a4ed3945a9eeefc18f1138637d5e
FujiApple <fujiapple852@gmail.com>
2024-02-10 16:09:03 +0800

    test(net): added ipv4 test `test_dispatch_icmp_probe_invalid_packet_size_high`

--- src/tracing/net/ipv4.rs
+++ src/tracing/net/ipv4.rs
@@ -512,15 +512,7 @@ mod tests {
     // Test dispatching a IPv4/ICMP probe.
     #[test]
     fn test_dispatch_icmp_probe_no_payload() -> anyhow::Result<()> {
-        let probe = Probe::new(
-            Sequence(33000),
-            TraceId(1234),
-            Port(0),
-            Port(0),
-            TimeToLive(10),
-            Round(0),
-            SystemTime::now(),
-        );
+        let probe = make_probe();
         let src_addr = Ipv4Addr::from_str("1.2.3.4")?;
         let dest_addr = Ipv4Addr::from_str("5.6.7.8")?;
         let packet_size = PacketSize(28);
@@ -556,6 +548,29 @@ mod tests {
         Ok(())
     }
 
+    #[test]
+    fn test_dispatch_icmp_probe_invalid_packet_size() -> anyhow::Result<()> {
+        let probe = make_probe();
+        let src_addr = Ipv4Addr::from_str("1.2.3.4")?;
+        let dest_addr = Ipv4Addr::from_str("5.6.7.8")?;
+        let packet_size = PacketSize(1025);
+        let payload_pattern = PayloadPattern(0x00);
+        let ipv4_byte_order = platform::PlatformIpv4FieldByteOrder::Network;
+        let mut mocket = MockSocket::new();
+        let err = dispatch_icmp_probe(
+            &mut mocket,
+            probe,
+            src_addr,
+            dest_addr,
+            packet_size,
+            payload_pattern,
+            ipv4_byte_order,
+        )
+        .unwrap_err();
+        assert!(matches!(err, TracerError::InvalidPacketSize(_)));
+        Ok(())
+    }
+
     // This IPv4/ICMP TimeExceeded packet has code 1 ("Fragment reassembly
     // time exceeded") and must be ignored.
     //
@@ -578,5 +593,17 @@ mod tests {
         let resp = recv_icmp_probe(&mut mocket, Protocol::Udp, IcmpExtensionParseMode::Enabled)?;
         assert!(resp.is_none());
         Ok(())
+    }
+
+    fn make_probe() -> Probe {
+        Probe::new(
+            Sequence(33000),
+            TraceId(1234),
+            Port(0),
+            Port(0),
+            TimeToLive(10),
+            Round(0),
+            SystemTime::now(),
+        )
     }
 }
