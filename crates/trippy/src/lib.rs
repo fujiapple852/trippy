518e0cd343e2d2c57b1d1751d1397c8b9e0b4c3c
FujiApple <fujiapple852@gmail.com>
2024-06-16 11:15:47 +0800

    feat: re-export `trippy-core` from `trippy` crate

--- crates/trippy/src/lib.rs
+++ crates/trippy/src/lib.rs
@@ -1,2 +1,5 @@
 #![allow(rustdoc::broken_intra_doc_links, rustdoc::bare_urls)]
 #![doc = include_str!("../../../README.md")]
+
+// Re-export the core library, so it may be used from trippy crate.
+pub use trippy_core::*;
