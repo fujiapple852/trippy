1684f7718c079b3d6597e34d70725c90b75d4ded
FujiApple <fujiapple852@gmail.com>
2024-08-09 22:39:31 +0800

    feat(tui): revamp the help dialog (#1260)

--- crates/trippy-tui/src/frontend/render/help.rs
+++ crates/trippy-tui/src/frontend/render/help.rs
@@ -8,44 +8,46 @@ use ratatui::Frame;
 
 /// Render help dialog.
 pub fn render(f: &mut Frame<'_>, app: &TuiApp) {
+    let s = app.tui_config.bindings.toggle_settings;
+    let b = app.tui_config.bindings.toggle_settings_bindings;
+    let c = app.tui_config.bindings.toggle_settings_columns;
+    #[allow(clippy::needless_raw_string_hashes)]
+    let help_lines = vec![
+        r#"                           "#.to_string(),
+        r#" _____    _                "#.to_string(),
+        r#"|_   _| _(_)_ __ _ __ _  _ "#.to_string(),
+        r#"  | || '_| | '_ \ '_ \ || |"#.to_string(),
+        r#"  |_||_| |_| .__/ .__/\_, |"#.to_string(),
+        r#"           |_|  |_|   |__/ "#.to_string(),
+        r#"                           "#.to_string(),
+        r#" A network diagnostic tool "#.to_string(),
+        r#"                           "#.to_string(),
+        format!(" Press [{s}] to show all settings "),
+        format!(" Press [{b}] to show key bindings "),
+        format!(" Press [{c}] to choose columns "),
+        r#"                           "#.to_string(),
+        r#" https://github.com/fujiapple852/trippy "#.to_string(),
+        r#"                           "#.to_string(),
+        r#" Distributed under the Apache License 2.0 "#.to_string(),
+        r#"                           "#.to_string(),
+        r#" Copyright 2022 Trippy Contributors "#.to_string(),
+    ];
     let block = Block::default()
-        .title(" Default Controls ")
+        .title(" Help ")
         .title_alignment(Alignment::Center)
         .borders(Borders::ALL)
         .style(Style::default().bg(app.tui_config.theme.help_dialog_bg))
         .border_type(BorderType::Double);
-    let control_line: Vec<_> = HELP_LINES.iter().map(|&line| Line::from(line)).collect();
+    let control_line: Vec<_> = help_lines
+        .iter()
+        .map(|line| Line::from(line.as_str()))
+        .collect();
     let control = Paragraph::new(control_line)
         .style(Style::default().fg(app.tui_config.theme.help_dialog_text))
         .block(block.clone())
-        .alignment(Alignment::Left);
+        .alignment(Alignment::Center);
     let area = util::centered_rect(60, 60, f.area());
     f.render_widget(Clear, area);
     f.render_widget(block, area);
     f.render_widget(control, area);
 }
-
-const HELP_LINES: [&str; 22] = [
-    "[up] & [down]    - select hop",
-    "[left] & [right] - select trace or flow",
-    ", & .            - select hop address",
-    "[esc]            - clear selection",
-    "d                - toggle hop details",
-    "f                - toggle flows",
-    "c                - toggle chart",
-    "m                - toggle map",
-    "p                - toggle privacy",
-    "Ctrl+f           - toggle freeze display",
-    "Ctrl+r           - reset statistics",
-    "Ctrl+k           - flush DNS cache",
-    "i                - show IP only",
-    "n                - show hostname only",
-    "b                - show both IP and hostname",
-    "[ & ]            - expand & collapse hosts",
-    "{ & }            - expand & collapse hosts to max and min",
-    "+ & -            - zoom chart in and out",
-    "z                - toggle AS information (if available)",
-    "h or ?           - toggle help",
-    "s                - toggle settings (jump to tab with 1-7 keys)",
-    "q                - quit",
-];
