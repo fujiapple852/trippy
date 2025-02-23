cf945e0141e25d319d3a7c4bd6d910afcad102ad
FujiApple <fujiapple852@gmail.com>
2025-02-23 12:43:28 +0800

    fix(core): buffer vec WIP

--- crates/trippy-core/src/strategy.rs
+++ crates/trippy-core/src/strategy.rs
@@ -841,7 +841,6 @@ mod state {
     use crate::strategy::{StrategyConfig, StrategyResponse};
     use crate::types::{MaxRounds, Port, RoundId, Sequence, TimeToLive, TraceId};
     use crate::{Flags, MultipathStrategy, PortDirection, Protocol};
-    use std::array::from_fn;
     use std::net::IpAddr;
     use std::time::SystemTime;
     use tracing::instrument;
@@ -877,7 +876,7 @@ mod state {
         /// Tracer configuration.
         config: StrategyConfig,
         /// The state of all `ProbeStatus` requests and responses.
-        buffer: [ProbeStatus; BUFFER_SIZE as usize],
+        buffer: Vec<ProbeStatus>,
         /// An increasing sequence number for every `EchoRequest`.
         sequence: Sequence,
         /// The starting sequence number of the current round.
@@ -905,7 +904,7 @@ mod state {
         pub fn new(config: StrategyConfig) -> Self {
             Self {
                 config,
-                buffer: from_fn(|_| ProbeStatus::default()),
+                buffer: vec![ProbeStatus::default(); BUFFER_SIZE as usize],
                 sequence: config.initial_sequence,
                 round_sequence: config.initial_sequence,
                 ttl: config.first_ttl,
