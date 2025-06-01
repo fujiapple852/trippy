fd00c075a18a140db9196f7bb0e8994c7d1cf791
FujiApple <fujiapple852@gmail.com>
2025-06-01 20:08:02 +0800

    docs(website): reformat

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,14 +5,14 @@ import starlightVersions from 'starlight-versions'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://trippy.rs',
-	integrations: [
-		starlight({
+    integrations: [
+        starlight({
             plugins: [
               starlightVersions({
                 versions: [{ slug: '0.12.2' }, { slug: '0.13.0' }],
               }),
             ],
-			title: 'Trippy',
+            title: 'Trippy',
             customCss: [
               // Relative path to your custom CSS file
               './src/styles/custom.css',
@@ -34,30 +34,30 @@ export default defineConfig({
                 },
               },
             ],
-			social: [
+            social: [
                 { icon: 'github', label: 'github', href: 'https://github.com/fujiapple852/trippy' },
                 { icon: 'zulip', label: 'zulip', href: 'https://trippy.zulipchat.com' },
                 { icon: 'matrix', label: 'matrix', href: 'https://matrix.to/#/#trippy-dev:matrix.org' },
                 { icon: 'x.com', label: 'x.com', href: 'https://x.com/FujiApple852v2' },
-			],
-			sidebar: [
+            ],
+            sidebar: [
                 {
                     label: 'Start Here',
                     autogenerate: { directory: 'start' }
                 },
-				{
+                {
                     label: 'Guides',
                     autogenerate: { directory: 'guides' }
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
                 {
                     label: 'Development',
                     autogenerate: { directory: 'development' },
                 },
-			],
-		}),
-	],
+            ],
+        }),
+    ],
 });
--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -3,6 +3,6 @@ import { docsSchema } from '@astrojs/starlight/schema';
 import { docsVersionsLoader } from 'starlight-versions/loader'
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	versions: defineCollection({ loader: docsVersionsLoader() }),
+    docs: defineCollection({ schema: docsSchema() }),
+    versions: defineCollection({ loader: docsVersionsLoader() }),
 };
