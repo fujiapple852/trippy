dc0da269ae2a89db819d5f55a2883be0fbb0dc96
FujiApple <fujiapple852@gmail.com>
2025-08-02 14:26:22 +0800

    build: update ubuntu ppa release script (#1623)

--- ubuntu-ppa/release.sh
+++ ubuntu-ppa/release.sh
@@ -5,24 +5,26 @@ set -o errexit -o pipefail -o nounset
 # The Trippy version to release
 VERSION="0.14.0-dev"
 
+# The upstream version to use in the PPA
+#
+# This should usually be the same as the VERSION, but if the upstream tarball has been repacked
+# (e.g. to remove unnecessary files or to fix the tarball) then this should be set to the upstream version
+# with a `+repack{N}` suffix, where `{N}` is the number of times the tarball has been repacked. i.e. `0.1.0+repack1`.
+UPSTREAM="0.14.0-dev"
+
 # The revision number for the PPA
+#
+# This is incremented each time a new upload is made to the PPA so will always be one greater than repack number.
 REVISION=1
 
 # The Ubuntu series to build for
 SERIES=("noble" "jammy")
 
-TARBALL="trippy_${VERSION}.orig.tar.gz"
+TARBALL="trippy_${UPSTREAM}.orig.tar.gz"
 PACKAGE="trippy"
 CHANGES="New upstream release"
 export DEBEMAIL="fujiapple852@gmail.com"
 export DEBFULLNAME="Fuji Apple"
-
-# Download TARBALL
-wget -O "${TARBALL}" "https://github.com/fujiapple852/trippy/archive/refs/tags/${VERSION}.tar.gz"
-if [[ ! -f "${TARBALL}" ]]; then
-    echo "Error: Failed to download TARBALL." >&2
-    exit 1
-fi
 
 # Import GPG key securely
 if [[ ! -f launchpad_secret_key.pgp ]]; then
@@ -40,9 +42,23 @@ if gpg --list-keys --with-colons "${GPG_KEY_ID}" | grep '^pub' | grep '[e]'; the
     exit 1
 fi
 
+# Download TARBALL
+wget -O "${TARBALL}" "https://github.com/fujiapple852/trippy/archive/refs/tags/${VERSION}.tar.gz"
+if [[ ! -f "${TARBALL}" ]]; then
+    echo "Error: Failed to download TARBALL." >&2
+    exit 1
+fi
+
 # Vendor the cargo dependencies
-cargo-1.82 vendor
-tar cJf ubuntu-ppa/vendor.tar.xz vendor
+# We have to ensure we run cargo `vendor --locked` against the src in the tarball, not the src in the current directory.
+tar -xf "${TARBALL}"
+pushd "trippy-${VERSION}"
+rm -f ../ubuntu-ppa/vendor.tar.xz
+rm -rf vendor
+cargo-1.82 vendor --locked
+tar -cJf ../ubuntu-ppa/vendor.tar.xz vendor
+popd
+rm -rf "trippy-${VERSION}"
 
 for series in "${SERIES[@]}"; do
     UBUNTU_VERSION=$(distro-info --series "${series}" -r | cut -d' ' -f1)
@@ -54,7 +70,7 @@ for series in "${SERIES[@]}"; do
     # Update changelog for the specific series
     rm -f debian/changelog
     dch --create --distribution "${series}" --PACKAGE "${PACKAGE}" \
-        --newversion "${VERSION}-ppa${REVISION}~ubuntu${UBUNTU_VERSION}" "$CHANGES"
+        --newversion "${UPSTREAM}-ppa${REVISION}~ubuntu${UBUNTU_VERSION}" "$CHANGES"
 
     # Build the source PACKAGE
     debuild --prepend-path ~/.cargo/bin -S -sa
